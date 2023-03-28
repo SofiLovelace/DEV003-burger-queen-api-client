cb3d0030c0af08f98299b720c6a56b4dd7a4a895
SofiLovelace <kaozztrs@gmail.com>
2023-03-28 12:50:24 -0600

    se agregan services

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { Observable } from "rxjs/Observable"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  
+  
+}
