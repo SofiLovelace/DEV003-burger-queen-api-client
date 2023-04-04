10dc206a11ee4f51ea6ca0c7d827b25a3ed82c21
“Nicole <nicolecondeduque@gmail.com>
2023-04-04 14:01:32 -0400

    cambios del css del componente del nav

--- src/app/views/login/auth.service.ts
+++ src/app/views/login/auth.service.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http'
 
 export class AuthService {
   private urlApiLocalHost = 'http://localhost:8080'
-  private urlApiWeb = 'https://burguerqueen-service.onrender.com'
+  private urlApiWeb = 'https://burgermock-api.onrender.com'
   constructor(private http: HttpClient) { }
   public get(path: string, body: object): any { // es importante typar lo que retornara una función
     return this.http.post(this.urlApiWeb + path, body) // el metodo http ya devuelve un observable
