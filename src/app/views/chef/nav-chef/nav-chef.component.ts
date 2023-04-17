475104b66bee56cd7a21b12143ab782a4c7547e8
“Nicole <nicolecondeduque@gmail.com>
2023-04-17 15:12:46 -0400

    se agrega componente chef

---
+++ src/app/views/chef/nav-chef/nav-chef.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-chef',
+  templateUrl: './nav-chef.component.html',
+  styleUrls: ['./nav-chef.component.css']
+})
+export class NavChefComponent {
+
+}
