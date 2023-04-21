8ec7073bfeb77ea62dd8f1f5e00dcdf2a7bac9f4
“Nicole <nicolecondeduque@gmail.com>
2023-04-21 12:00:21 -0400

    se agregaron diseno y funcion de botones chef

--- src/app/views/chef/order/order.component.ts
+++ src/app/views/chef/order/order.component.ts
@@ -1,89 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { IResponseOrder } from 'src/app/models/views/chef.interface';
 import { HttpsService } from 'src/app/services/https.service';
 import { SwitchService } from 'src/app/services/switch.service';
 
 @Component({
-  selector: 'app-order',
-  templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+    selector: 'app-order',
+    templateUrl: './order.component.html',
+    styleUrls: ['./order.component.css']
 })
 
 export class OrderComponent {
 
-  public dataOrders: IResponseOrder[] = [] // generamos un array que modidifcaremos con la data que recibamos del servidor
-  public orderFilter: IResponseOrder[] = []
 
-  public modalSwitch: boolean = false
+    @ViewChild('btnPending') btnPending!: ElementRef;
+    @ViewChild('btnComplete') btnComplete!: ElementRef;
 
-  constructor (
-    private HttpsService: HttpsService,
-    public switchS: SwitchService
-  ) {}
-  
-  public filterOrder(status: 'complete' | 'pending') {
-    this.orderFilter = this.dataOrders.filter(order => order.status === status)
-  }
 
-  private getOrders (): void {
-    this.HttpsService.get('orders')
-    .subscribe({
-      next: (response: IResponseOrder[]) => {
-        this.dataOrders = response.sort((a,b) => a.dataEntry - b.dataEntry)
-        this.filterOrder('pending')
-      },
-      error: (err: any)=> {
-        console.log('error',err) // gestion de errores
-      },
-      complete:()=> {
-        console.log('complete') // codigo correcto
-      }
-    })
-  }
+    public dataOrders: IResponseOrder[] = [] // generamos un array que modidifcaremos con la data que recibamos del servidor
+    public orderFilter: IResponseOrder[] = []
 
-  public finishOrder(data: any):void {
-    const dataFinish = {
-      id: data.id,
-      userId: data.userId,
-      client: data.client,
-      products: data.products,
-      status: data.status,
-      dataEntry: data.dataEntry,
-  }
+    public modalSwitch: boolean = false
 
-    setTimeout(() => {
-      this.switchS.$dataOrder.emit(dataFinish)
-    }, 1)
-  }
+    constructor(
+        private HttpsService: HttpsService,
+        public switchS: SwitchService,
+        private renderer2: Renderer2
+    ) { }
 
-  public openModal(data: any) {
-    this.modalSwitch = true
-    this.finishOrder(data)
-  }
-
-  public completeOrder (id:number) { // funcion que marca el pedido como ccompletado
-    const bodyHttp = {
-      dataFinish: new Date(),
-      status: 'complete'
+    public filterOrder(status: 'complete' | 'pending') {
+        this.orderFilter = this.dataOrders.filter(order => order.status === status)
     }
-    this.HttpsService.patch('orders' + '/' + id, bodyHttp)
-    .subscribe({
-      next: (response: any) => {
-        console.log('respuesta', response)
-      }
-      ,
-      error: (err: any)=> {
-        console.log('error',err) // gestion de errores
-      },
-      complete:()=> {
-        this.getOrders()
-        console.log('complete') // codigo correcto
-      }
-    })
-  }
 
-  ngOnInit():void {
-    this.getOrders()
-    this.switchS.$switchModal.subscribe((res) => this.modalSwitch = res)
-  }
+    private getOrders(): void {
+        this.HttpsService.get('orders')
+            .subscribe({
+                next: (response: IResponseOrder[]) => {
+                    this.dataOrders = response.sort((a, b) => a.dataEntry - b.dataEntry)
+                    this.filterOrder('pending')
+                },
+                error: (err: any) => {
+                    console.log('error', err) // gestion de errores
+                },
+                complete: () => {
+                    console.log('complete') // codigo correcto
+                }
+            })
+    }
+
+    public finishOrder(data: any): void {
+        const dataFinish = {
+            id: data.id,
+            userId: data.userId,
+            client: data.client,
+            products: data.products,
+            status: data.status,
+            dataEntry: data.dataEntry,
+        }
+
+        setTimeout(() => {
+            this.switchS.$dataOrder.emit(dataFinish)
+        }, 1)
+    }
+
+    public openModal(data: any) {
+        this.modalSwitch = true
+        this.finishOrder(data)
+    }
+
+    public completeOrder(id: number) { // funcion que marca el pedido como ccompletado
+        const bodyHttp = {
+            dataFinish: new Date(),
+            status: 'complete'
+        }
+        this.HttpsService.patch('orders' + '/' + id, bodyHttp)
+            .subscribe({
+                next: (response: any) => {
+                    console.log('respuesta', response)
+                }
+                ,
+                error: (err: any) => {
+                    console.log('error', err) // gestion de errores
+                },
+                complete: () => {
+                    this.getOrders()
+                    console.log('complete') // codigo correcto
+                }
+            })
+    }
+
+    public changeColor(button: string) {
+        const btnPending = this.btnPending.nativeElement;
+        const btnComplete = this.btnComplete.nativeElement;
+
+        if (button === "pendiente") {
+            this.renderer2.setStyle(btnPending, 'backgroundColor', 'black');
+            this.renderer2.setStyle(btnComplete, 'backgroundColor', 'gray');
+        } else {
+            this.renderer2.setStyle(btnPending, 'backgroundColor', 'gray');
+            this.renderer2.setStyle(btnComplete, 'backgroundColor', 'black');
+        }
+    }
+
+    ngOnInit(): void {
+        this.getOrders()
+        this.switchS.$switchModal.subscribe((res) => this.modalSwitch = res)
+    }
 }
