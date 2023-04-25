fd69de0bd0a544b7c1181d11a8257e91ac2c20c0
SofiLovelace <kaozztrs@gmail.com>
2023-04-25 11:41:08 -0600

    probando rutas hijas en waiter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,26 @@ import { loginComponent } from './views/login/login.component';
 import { WaiterComponent } from './views/waiter/waiter.component';
 import { ChefComponent } from './views/chef/chef.component';
 import { AdminComponent } from './views/admin/admin.component';
-import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component'
-
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'login', component: loginComponent },
-  {path: 'waiter', component: WaiterComponent },
-  {path: 'chef', component: ChefComponent },
-  {path: 'admin', component: AdminComponent },
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: '**', component: PageNotFoundComponent}
+  { path: 'login', component: loginComponent },
+  {
+    path: 'waiter',
+    component: WaiterComponent,
+    children: [
+      { path: '', component: WaiterComponent },
+      { path: 'oredersReady', component: WaiterComponent },
+    ],
+  },
+  { path: 'chef', component: ChefComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
