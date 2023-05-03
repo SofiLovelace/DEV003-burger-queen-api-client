6f86e5abaab09bf15615d618c4f49385e86832cd
SofiLovelace <kaozztrs@gmail.com>
2023-05-03 12:16:46 -0600

    descomentamos el guardian

--- src/app/guardian.guard.ts
+++ src/app/guardian.guard.ts
@@ -1,40 +1,40 @@
-//import { Injectable } from '@angular/core';
-//import {
-//  ActivatedRouteSnapshot,
-//  CanActivate,
-//  Router,
-//  RouterStateSnapshot,
-//  UrlTree,
-//} from '@angular/router';
-//import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
 
-//@Injectable({
-//  providedIn: 'root',
-//})
-//export class GuardianGuard implements CanActivate {
-//  constructor(private router: Router) {}
+@Injectable({
+  providedIn: 'root',
+})
+export class GuardianGuard implements CanActivate {
+  constructor(private router: Router) {}
 
-//  public checkPermissions(path: any): boolean {
-//    if (sessionStorage.getItem('userRole') === 'admin') {
-//      return true;
-//    }
-//    if (path === sessionStorage.getItem('userRole')) {
-//      return true;
-//    } else {
-//      this.router.navigate(['/login']);
-//      return false;
-//    }
-//  }
+  public checkPermissions(path: any): boolean {
+    if (sessionStorage.getItem('userRole') === 'admin') {
+      return true;
+    }
+    if (path === sessionStorage.getItem('userRole')) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
 
-//  canActivate(
-//    route: ActivatedRouteSnapshot,
-//    state: RouterStateSnapshot
-//  ):
-//    | Observable<boolean | UrlTree>
-//    | Promise<boolean | UrlTree>
-//    | boolean
-//    | UrlTree {
-//    console.log(route.url[0].path);
-//    return this.checkPermissions(route.url[0].path);
-//  }
-//}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    console.log(route.url[0].path);
+    return this.checkPermissions(route.url[0].path);
+  }
+}
