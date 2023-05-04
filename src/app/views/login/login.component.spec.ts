213bf18966d8809f51dd787a7bafeb1ef685dd2b
SofiLovelace <kaozztrs@gmail.com>
2023-05-04 09:42:01 -0600

    primeros tests de componente login

--- src/app/views/login/login.component.spec.ts
+++ src/app/views/login/login.component.spec.ts
@@ -3,7 +3,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { loginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-describe('loginComponent', () => {
+describe('test of loginComponent', () => {
   let component: loginComponent;
   let fixture: ComponentFixture<loginComponent>;
 
@@ -20,5 +20,20 @@ describe('loginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should to be invalid', () => {
+    const email = component.credential.controls['email'];
+    email.setValue;
+    ('sofi@gmail.com');
+    expect(component.credential.invalid).toBeTrue();
+  });
+
+  it('should to be invalid', () => {
+    const email = component.credential.controls['email'];
+    const password = component.credential.controls['password'];
+    email.setValue('sofi@gmail.com');
+    password.setValue('123456');
+    expect(component.credential.invalid).toBeFalse();
   });
 });
